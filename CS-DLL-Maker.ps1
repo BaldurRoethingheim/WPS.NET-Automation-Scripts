f95de84aee1b62f5d706a850a3168097080c18bb
Baldur <114744176+BaldurRoethingheim@users.noreply.github.com>
2026-02-26 04:26:48 +0100

    Fix typo in comment for CS-DLL-Maker.ps1

---
+++ CS-DLL-Maker.ps1
@@ -0,0 +1,3 @@
+# Help with dialog to make a C#.NET DLL with csc.exe
+# either query within Windows PowerShell (or with WinForms) and VSCode
+# preferable compile AOT and Single-File-Output with self-contained (bloated)
